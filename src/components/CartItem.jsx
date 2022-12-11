2b34288c196bd1cdb64332bfa615bf8f41f84186
akasaul <97656710+akasaul@users.noreply.github.com>
2022-12-11 17:26:20 +0000

    cart count added

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,5 +1,8 @@
 import CloseIcon from "@mui/icons-material/Close";
+import { useDispatch } from "react-redux";
+import { increment, decrement, removeItem } from "../features/Cart/CartSlice";
 const CartItem = ({ item }) => {
+  const dispatch = useDispatch();
   return (
     <section className="p-2 flex  sm:flex-row bg-white items-center sm:items-stretch justify-evenly space-x-6">
       <div className="w-32">
@@ -17,18 +20,18 @@ const CartItem = ({ item }) => {
         <h2 className="font-bold sm:text-3xl text-lg">Men Gucc Luiv Carpet</h2>
         <h3>{item.category}</h3>
         <div className="flex">
-          <button className="bg-gray-300 font-bold px-2 py-1 m-2 focus:outline-none rounded-full">
+          <button onClick={() => dispatch(increment(item.id))} className="bg-gray-300 font-bold px-2 py-1 m-2 focus:outline-none rounded-full">
             +
           </button>
           <button>{item.count}</button>
-          <button className="bg-gray-300 font-bold px-2 py-1 m-2 focus:outline-none rounded-full">
+          <button onClick={() => dispatch(decrement(item.id))} className="bg-gray-300 font-bold px-2 py-1 m-2 focus:outline-none rounded-full">
             -
           </button>
         </div>
       </div>
       <div>
         <h3 className="sm:text-2xl text-lg font-bold mb-5">${item.price}</h3>
-        <button className="p-2 bg-red-500 text-white rounded-2xl">
+        <button className="p-2 bg-red-500 text-white rounded-2xl" onClick={() => dispatch(removeItem(item.id))}>
           Remove
         </button>
       </div>
--- src/features/Cart/CartSlice.jsx
+++ src/features/Cart/CartSlice.jsx
@@ -7,10 +7,24 @@ export const cartSlice = createSlice({
     },
     reducers: {
         addToCart: (state, payload) => {
-            state.value.push(payload.payload);
-        }
+            const item = state.value.find((item) => item.id === payload.payload.id);
+            if(item) {
+                item.count++;
+            } else {
+                state.value.push(payload.payload);
+            }
+        },
+        decrement: (state, payload) => {
+            const item = state.value.find((item) => item.id === payload);
+            item.count++;
+        },
+        increment: (state, payload) => {
+            const item = state.value.find((item) => item.id === payload);
+            item.count--;
+        },
+        removeItem: (state, payload) => state.value.filter((item) => item.id !== payload)          
     }
 })
 
-export const {addToCart} = cartSlice.actions;
+export const {addToCart, increment, decrement, removeItem} = cartSlice.actions;
 export default cartSlice.reducer;
